54bfc674e03fd4990e98e8954255e10865d36403
abdelalaoui <105356456+abdelalaoui@users.noreply.github.com>
2023-10-26 00:41:33 +0100

    1

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,9 +11,7 @@ export const Footer = () => {
       <Container>
         <Row className="align-items-center">
           <MailchimpForm />
-          <Col size={12} sm={6}>
-            <img src={logo} alt="Logo" />
-          </Col>
+          
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
               <a href="https://www.linkedin.com/in/abdallah-alaoui-16a551249/"><img src={navIcon1} alt="Icon" /></a>
